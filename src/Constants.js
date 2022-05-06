204a5bf2485b526a9b79e37222c7ef8ebdb0f523
MichaelHulme <coachhulmeumb@gmail.com>
2022-05-06 12:29:59 -0400

    added build

--- src/Constants.js
+++ src/Constants.js
@@ -1,11 +1,11 @@
 const prod = {
     url: {
-     API_URL: 'https://little-legumes.herokuapp.com'
+     API_URL: process.env.PROD_API_URL
      }
    };
    const dev = {
     url: {
-     API_URL: 'http://localhost:5000'
+     API_URL: process.env.DEV_API_URL
     }
    };
    export const apiConfig = process.env.NODE_ENV === 'development' ? dev : prod;
